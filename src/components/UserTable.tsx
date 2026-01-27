9e8557353a9cf343de898010b2b723edbb268242
Elite Dev <oyenekantomiwa2.com>
2026-01-27 23:50:41 +0100

    minor fix to typpe error

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { columns } from "./Columns";
+import { columns, type Payment } from "./Columns";
 import { DataTable } from "./DataTable";
 import usersData from "../db.json";
 
 function UserTable() {
-  const data = usersData;
+  const data = usersData as Payment[];
 
   return (
     <div className="mt-10 bg-white p-6 rounded-sm shadow-sm border border-gray-100">
