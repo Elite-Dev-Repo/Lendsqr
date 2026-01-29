0c0f5a558b56f08cef2f0897c15888f7f2f046c3
Elite Dev <oyenekantomiwa2.com>
2026-01-30 00:46:46 +0100

    error fix2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-// import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
 import Login from "./Login.tsx";
 import Dashboard from "./Dashboard.tsx";
@@ -13,10 +12,6 @@ const router = createBrowserRouter([
     path: "/",
     element: <Login />,
   },
-  // {
-  //   path: "/login",
-  //   element: <Login />,
-  // },
   {
     path: "/dashboard",
     element: <Dashboard />,
